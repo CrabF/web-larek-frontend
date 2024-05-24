e96df811cbd130931db168f4d9579645682a859c
dmitry.mazhnik <dmitry-mazhnik@yandex.ru>
2024-05-24 14:09:51 +0300

    add new method in Model and some testing code

--- src/components/common/AppData.ts
+++ src/components/common/AppData.ts
@@ -1,8 +1,10 @@
 import { IOrder, IOrderSuccess, IProductItem, IProductList } from "../../types";
+import { IEvents } from "../base/events";
 
 
 export interface IAppData{
-  getCards(): IProductList;
+  getCards(): IProductItem[];
+  setCards(items: IProductItem[]): void;
   getCard(id: string): IProductItem;
   getTotal(): number;
   getOrder(): IProductItem[];
@@ -13,5 +15,22 @@ export interface IAppData{
 }
 
 export class AppData implements IAppData{
-  
+  protected items: IProductItem[]=[];
+  // constructor(protected events: IEvents){}
+  constructor(){}
+  getCards() {
+    return this.items;
+  }
+
+  setCards(items: IProductItem[]){
+    return this.items = items;
+  }
+
+  getCard(id: string): IProductItem;
+  getTotal(): number;
+  getOrder(): IProductItem[];
+  removeFromOrder(id: string): void;
+  addToOrder(id: string): void;
+  validateForm(form: HTMLElement): void;
+  successOrder(order: IOrder): IOrderSuccess;
 }
