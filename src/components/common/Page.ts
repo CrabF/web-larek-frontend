09ff10d0c825206387185b7a169f119ee7f3e1d3
dmitry.mazhnik <dmitry-mazhnik@yandex.ru>
2024-05-27 19:59:26 +0300

    add new class Page for View and upd reamde

---
+++ src/components/common/Page.ts
@@ -0,0 +1,34 @@
+import { Component } from "../base/Components";
+import { IEvents } from "../base/events";
+import { ensureElement } from "../../utils/utils";
+
+interface IPage {
+  counter: number;
+  catalog: HTMLElement[];
+} 
+
+export class Page extends Component<IPage> {
+  protected basketCounter: HTMLElement;
+  protected gallery: HTMLElement;
+  protected basket: HTMLElement;
+
+  constructor(documentBody: HTMLElement, protected events: IEvents){
+    super(documentBody);
+    this.basketCounter = ensureElement<HTMLElement>('.header__basket-counter');
+    this.gallery = ensureElement<HTMLElement>('.gallery');
+    this.basket = ensureElement<HTMLElement>('.header__basket');
+    this.basket.addEventListener('click', ()=>{
+      events.emit('basket:open')
+    })
+    }
+
+    set counter(value: number) {
+
+    }
+
+    set catalog(items: HTMLElement[]) {
+
+    }
+  }
+
+ 
